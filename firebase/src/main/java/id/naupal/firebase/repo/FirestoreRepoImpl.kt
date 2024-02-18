2581dea0f2392f98730296c2be12b7ef1ac963a1
NaupalTrianto <naupal.trianto@mapan.id>
2024-02-18 15:51:34 +0700

    handle unsupported firestore query

--- firebase/src/main/java/id/naupal/firebase/repo/FirestoreRepoImpl.kt
+++ firebase/src/main/java/id/naupal/firebase/repo/FirestoreRepoImpl.kt
@@ -6,6 +6,7 @@ import id.naupal.firebase.model.ResultFbState
 import id.naupal.firebase.model.TicketDto
 import id.naupal.firebase.model.TicketFiler
 import id.naupal.firebase.repo.FirestoreRepoImpl.Sort.Companion.fromLabel
+import java.lang.Exception
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
@@ -67,44 +68,49 @@ class FirestoreRepoImpl : FirestoreRepo {
 
     override suspend fun getTickets(sortBy: String, filter: TicketFiler?): ResultFbState =
         suspendCoroutine { continuation ->
+            try {
+                var collRef = Firebase.firestore.collection(ticketColl)
+                    .orderBy(fromLabel(sortBy).value.second)
 
-            var collRef = Firebase.firestore.collection(ticketColl)
-                .orderBy(fromLabel(sortBy).value.second)
+                filter?.let {
+                    if (it.starDate > 0) {
+                        collRef = collRef.whereGreaterThanOrEqualTo(dateTimeField, it.starDate)
+                    }
+                    if (it.endDate > 0) {
+                        collRef = collRef.whereLessThanOrEqualTo(dateTimeField, it.endDate)
+                    }
+                    if (it.driverName.isNotEmpty()) {
+                        collRef = collRef.whereEqualTo(driverNameField, it.driverName)
+                    }
+                    if (it.licenseNumber.isNotEmpty()) {
+                        collRef = collRef.whereEqualTo(licenceNumberField, it.licenseNumber)
+                    }
+                }
 
-            filter?.let {
-                if (it.starDate > 0) {
-                    collRef = collRef.whereGreaterThanOrEqualTo(dateTimeField, it.starDate)
-                }
-                if (it.endDate > 0) {
-                    collRef = collRef.whereLessThanOrEqualTo(dateTimeField, it.endDate)
-                }
-                if (it.driverName.isNotEmpty()) {
-                    collRef = collRef.whereEqualTo(driverNameField, it.driverName)
-                }
-                if (it.licenseNumber.isNotEmpty()) {
-                    collRef = collRef.whereEqualTo(licenceNumberField, it.licenseNumber)
-                }
-            }
-
-            collRef.get().addOnSuccessListener { result ->
-                val tickets = result.map { doc ->
-                    TicketDto(
-                        id = doc.getString(idField) ?: "",
-                        dateTime = doc.getLong(dateTimeField) ?: 0,
-                        licenceNumber = doc.getString(licenceNumberField) ?: "",
-                        driverName = doc.getString(driverNameField) ?: "",
-                        inboundWeight = doc.getLong(inboundWeightField) ?: 0,
-                        outboundWeight = doc.getLong(outboundWeightField) ?: 0,
-                        netWeight = doc.getLong(netWeightField) ?: 0
+                collRef.get().addOnSuccessListener { result ->
+                    val tickets = result.map { doc ->
+                        TicketDto(
+                            id = doc.getString(idField) ?: "",
+                            dateTime = doc.getLong(dateTimeField) ?: 0,
+                            licenceNumber = doc.getString(licenceNumberField) ?: "",
+                            driverName = doc.getString(driverNameField) ?: "",
+                            inboundWeight = doc.getLong(inboundWeightField) ?: 0,
+                            outboundWeight = doc.getLong(outboundWeightField) ?: 0,
+                            netWeight = doc.getLong(netWeightField) ?: 0
+                        )
+                    }
+                    continuation.resume(ResultFbState.Success.GetTickets(tickets))
+                }.addOnFailureListener { exception ->
+                    continuation.resume(
+                        ResultFbState.Error(
+                            throwable = exception.cause,
+                            message = exception.message
+                        )
                     )
                 }
-                continuation.resume(ResultFbState.Success.GetTickets(tickets))
-            }.addOnFailureListener { exception ->
+            } catch (ex: Exception) {
                 continuation.resume(
-                    ResultFbState.Error(
-                        throwable = exception.cause,
-                        message = exception.message
-                    )
+                    ResultFbState.Error(message = ex.message)
                 )
             }
         }
