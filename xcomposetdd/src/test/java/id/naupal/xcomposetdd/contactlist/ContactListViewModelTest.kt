c1d7a144d47af0e944bde58bfd42c96676bcc4e9
NaupalTrianto <naupal.trianto@mapan.id>
2024-05-08 12:21:23 +0700

    tdd #1 Create the ViewModel test class

---
+++ xcomposetdd/src/test/java/id/naupal/xcomposetdd/contactlist/ContactListViewModelTest.kt
@@ -0,0 +1,15 @@
+package id.naupal.xcomposetdd.contactlist
+
+class ContactListViewModelTest {
+
+    fun `test that loading indicator visibility state is true when ViewModel first load`() {}
+
+    fun `test that loading indicator visibility state is false when fetch contacts finished successful`() {}
+
+    fun `test that loading indicator visibility state is false when fetch contacts returns error`() {}
+
+    fun `test that contacts data should be updated when fetch contacts successful`() {}
+
+    fun `test that error layout visibility is true when fetch contacts throws error`() {}
+
+}
